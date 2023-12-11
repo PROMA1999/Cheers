50d8a46ad3f464de45a04325b381d69f66212493
PROMA1999 <pablorodriguezmaden@hotmail.com>
2023-12-11 15:40:37 +0100

    j'ai fais la séparation des fichier poujr wp

--- header.php
+++ header.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cheers</title>
+    <?php wp_head(); ?> 
+    
 </head>
 <body>
-    <h1> Hello </h1>
-    <h2>hELLO WORLD</h2>
-</body>
-</html>
